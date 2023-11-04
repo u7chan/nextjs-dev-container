6cd3f790f3d8f94efc76f1a1546bed2e2176b1bc
una <unagamix@gmail.com>
2023-11-04 05:09:40 +0000

    refactor: copyright component

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,25 +1,12 @@
 import * as React from 'react'
 import Avatar from '@mui/material/Avatar'
 import CssBaseline from '@mui/material/CssBaseline'
-import Link from '@mui/material/Link'
 import Box from '@mui/material/Box'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import SigninForm from '@/components/SigninForm'
-
-function Copyright(props: any) {
-  return (
-    <Typography variant='body2' color='text.secondary' align='center' {...props}>
-      {'Copyright © '}
-      <Link color='inherit' href='https://mui.com/'>
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
+import Copyright from '@/components/Copyright'
 
 export default function Page() {
   return (
