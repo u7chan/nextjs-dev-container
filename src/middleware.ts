51392ea653af4ed41942383d34b0ea045612668b
u7chan <t.unagami.dev@gmail.com>
2023-11-23 23:24:42 +0900

    fix: add comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+// issues: https://github.com/vercel/next.js/issues/56398
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|forgot-password).*)'],
@@ -6,6 +9,6 @@ export const config = {
 
 export default withAuth({
   pages: {
-    signIn: '/login',
+    signIn: '/login', // Default signIn URL: http://localhost:3000/api/auth/signin
   },
 })
