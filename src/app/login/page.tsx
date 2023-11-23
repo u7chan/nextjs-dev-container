6e36b2ec8cc7abc5a06157325412440b8d6c5732
u7chan <t.unagami.dev@gmail.com>
2023-11-23 17:00:43 +0900

    clean

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,3 @@
-import { redirect } from 'next/navigation'
 import Avatar from '@mui/material/Avatar'
 import CssBaseline from '@mui/material/CssBaseline'
 import Box from '@mui/material/Box'
@@ -7,13 +6,8 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import LoginForm from '@/components/LoginForm'
 import Copyright from '@/components/Copyright'
-import { getUserSession } from '@/auth/getUserSession'
 
 export default async function Page() {
-  const userSession = await getUserSession()
-  if (userSession) {
-    redirect('/')
-  }
   return (
     <Container component='main' maxWidth='xs'>
       <CssBaseline />
