2fe57839e6a52e7b746684f7b5f35b55e501480a
u7chan <t.unagami.dev@gmail.com>
2023-11-23 17:24:43 +0900

    fix: matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "next-auth/middleware"
 
-export const config = { matcher: ["/"] }
+export const config = { matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"] }
 
 export default withAuth({
     pages: {
