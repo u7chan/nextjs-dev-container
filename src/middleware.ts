cf05bdb1f405afb90661196c12516e4e182e24e4
u7chan <t.unagami.dev@gmail.com>
2023-11-23 23:30:51 +0900

    fix: add comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
-// issues: https://github.com/vercel/next.js/issues/56398
+// Literal templates for `matcher` not working
+// see: https://github.com/vercel/next.js/issues/56398
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|forgot-password).*)'],
@@ -9,6 +10,7 @@ export const config = {
 
 export default withAuth({
   pages: {
-    signIn: '/login', // Default signIn URL: http://localhost:3000/api/auth/signin
+    signIn: '/login', // Default: http://localhost:3000/api/auth/signin
+    //signOut: '', // Default: http://localhost:3000/api/auth/signout
   },
 })
