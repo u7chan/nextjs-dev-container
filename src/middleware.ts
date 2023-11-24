d8f74f6e3077435103c1d2ad7361de25c8e11cf6
u7chan <t.unagami.dev@gmail.com>
2023-11-24 16:13:49 +0900

    fix: add comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+//import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
 
 // Literal templates for `matcher` not working
@@ -12,3 +13,10 @@ export default withAuth({
     signIn: '/login',
   },
 })
+
+// NOTE: middleware メソッドを定義すると next-auth の　middleware（認証）が機能しなくなる
+//
+// export function middleware(request: Request) {
+//   console.log('# middleware: ', request.url)
+//   NextResponse.next()
+// }
