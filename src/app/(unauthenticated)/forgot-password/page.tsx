962bc13f1ba2b5547a61f3a1dae59a97f7623a0d
u7chan <t.unagami.dev@gmail.com>
2023-11-24 16:04:56 +0900

    fix: page structure

--- src/app/(unauthenticated)/forgot-password/page.tsx
+++ src/app/(unauthenticated)/forgot-password/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { getUserSession } from '@/auth/getUserSession'
-import Layout from './_components/layout'
+import Layout from './_components'
 
 export default async function Page() {
   if (await getUserSession()) {
--- src/app/(unauthenticated)/login/page.tsx
+++ src/app/(unauthenticated)/login/page.tsx
@@ -1,36 +1,10 @@
 import { redirect } from 'next/navigation'
-import Avatar from '@mui/material/Avatar'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import LoginForm from '@/components/LoginForm'
-import Copyright from '@/components/Copyright'
 import { getUserSession } from '@/auth/getUserSession'
+import Layout from './_components'
 
 export default async function Page() {
   if (await getUserSession()) {
     redirect('/')
   }
-  return (
-    <Container component='main' maxWidth='xs'>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Log in to your account
-        </Typography>
-        <LoginForm />
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
-  )
+  return <Layout />
 }
