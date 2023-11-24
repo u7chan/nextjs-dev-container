dbf6a8f42df81462abba9f792e58eaf2095d6a86
u7chan <t.unagami.dev@gmail.com>
2023-11-24 15:11:59 +0900

    fix: api delay

--- src/app/api/(unauthenticated)/forgot-password/route.ts
+++ src/app/api/(unauthenticated)/forgot-password/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export async function POST() {
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
   return NextResponse.json({})
 }
